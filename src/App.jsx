0b258f61a828629a28de76fc01be8f6deb9e3426
iamajjayya <ajjayya2002@gmail.com>
2024-08-11 21:41:46 +0530

    gym

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import { Route , RouterProvider } from 'react-router-dom'
+import { Box } from '@mui/material'
+
+function App() {
+  
+  return (
+    <>
+        
+    </>
+  )
+}
+
+export default App
